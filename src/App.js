f68bae411d1dbe8ac566b130762fc936d2aa01fd
Sal Ferrarello <sal@ironcodestudio.com>
2020-11-27 12:01:11 -0500

    TEMP: Render a single 10 of hearts

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Card from './Card';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Card suit="hearts" value="10" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
