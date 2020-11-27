e016f7f55a9a9986f0f2b59c7713ec9a0614755e
Sal Ferrarello <sal@ironcodestudio.com>
2020-11-27 13:20:05 -0500

    TEMP Add example Hand to App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
-import Card from './Card';
+import Hand from './Hand';
 
 function App() {
+  const myHand = [
+    {
+      value: 'j',
+      suit: 'spades',
+    },
+    {
+      value: '6',
+      suit: 'spades',
+    },
+    {
+      value: 'k',
+      suit: 'spades',
+    },
+    {
+      value: 'k',
+      suit: 'hearts',
+    },
+    {
+      value: 'q',
+      suit: 'spades',
+    },
+    {
+      value: '1',
+      suit: 'clubs',
+    },
+    {
+      value: '4',
+      suit: 'diamonds',
+    },
+    {
+      value: '1',
+      suit: 'spades',
+    },
+  ];
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Card suit="hearts" value="10" />
+        <Hand cards={myHand} />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
