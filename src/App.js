4f1adb1766d588de4684b76d7f3a5d8f1c8acdbb
Sal Ferrarello <sal@ironcodestudio.com>
2020-11-28 10:49:03 -0500

    Remove logo.svg

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import Hand from './Hand';
 
@@ -40,7 +39,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <Hand cards={myHand} />
         <p>
           Edit <code>src/App.js</code> and save to reload.
