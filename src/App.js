3ce284a4dfa98ea238d9935d689970c0124cb7f8
Sal Ferrarello <sal@ironcodestudio.com>
2020-11-29 13:54:06 -0500

    Modify App.js to use Deck class

--- src/App.js
+++ src/App.js
@@ -1,44 +1,15 @@
 import './App.css';
 import Hand from './Hand';
 
+import Deck from './objects/Deck.js';
+
 function App() {
-  const myHand = [
-    {
-      value: 'j',
-      suit: 'spades',
-    },
-    {
-      value: '6',
-      suit: 'spades',
-    },
-    {
-      value: 'k',
-      suit: 'spades',
-    },
-    {
-      value: 'k',
-      suit: 'hearts',
-    },
-    {
-      value: 'q',
-      suit: 'spades',
-    },
-    {
-      value: '1',
-      suit: 'clubs',
-    },
-    {
-      value: '4',
-      suit: 'diamonds',
-    },
-    {
-      value: '1',
-      suit: 'spades',
-    },
-  ];
+  const deck = new Deck();
+  // Get 10 cards, for player in position 0, with 3 players in the game.
+  const dealtCards = deck.deal(10,0,3);
   return (
     <div className="App">
-        <Hand cards={myHand} />
+      <Hand cards={dealtCards} />
     </div>
   );
 }
