5e5e674824db39295423570d67d9214d5beb5819
Sal Ferrarello <sal@ironcodestudio.com>
2020-12-12 10:18:38 -0500

    Modify App.js to use PlayerCards component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import './App.css';
-import Hand from './Hand';
+import PlayerCards from './PlayerCards';
 
 import Deck from './objects/Deck.js';
 
 function App() {
-  const deck = new Deck();
-  // Get 10 cards, for player in position 0, with 3 players in the game.
-  const dealtCards = deck.deal(10,0,3);
+  const cardsForEachHand = [
+    9, 9, 9, 6,
+  ];
+  const hands = [];
+  const numHands = cardsForEachHand.length;
+  for (let i=0; i<numHands; i++) {
+    const deck = new Deck(),
+      numCards = cardsForEachHand[i],
+      playerPosition = 0,
+      playerCount = 5;
+
+    hands.push(
+      deck.deal(numCards, playerPosition, playerCount)
+    );
+  }
   return (
     <div className="App">
-      <Hand cards={dealtCards} />
+      <PlayerCards hands={hands} />
     </div>
   );
 }
