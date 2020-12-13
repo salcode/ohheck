0450b19e7d1e0be4f2834af8df64a0dfa991e582
Sal Ferrarello <sal@ironcodestudio.com>
2020-12-13 11:44:11 -0500

    Add checks on incoming parameters
    See #3

--- src/utilities/isValidUrlParams.js
+++ src/utilities/isValidUrlParams.js
@@ -1,5 +1,69 @@
 function isValidUrlParams(obj, hash) {
-  // Stub.
+  const {
+    numCardsEachRound,
+    playerIndex,
+    totalPlayers,
+  } = obj;
+  if (
+    playerIndex < 0 ||
+    playerIndex >= totalPlayers
+  ) {
+    console.error(
+      'playerIndex is outside of range',
+      {
+        playerIndex,
+        totalPlayers,
+      },
+    );
+    return false;
+  }
+
+  if (!numCardsEachRound.length) {
+    console.error(
+      'No rounds of cards are set to be dealt',
+      {
+        numCardsEachRound,
+      }
+    );
+    return false;
+  }
+
+  if (
+    ! numCardsEachRound
+      .slice(-1)
+      .every(
+        (numCards) => numCards * totalPlayers <= 52
+      )
+  ) {
+    console.error(
+      'The last round requires too many cards',
+      {
+        numCards: numCardsEachRound[
+          numCardsEachRound.length - 1
+        ],
+        totalPlayers,
+      }
+    );
+    return false;
+  }
+
+  if (
+    ! numCardsEachRound
+      .slice(0,-1).every(
+        (numCards) =>
+          (numCards * totalPlayers + 1) < 52
+      )
+  ) {
+    console.error(
+      'At least one round requires too many cards',
+      {
+        numCardsEachRound,
+        totalPlayers,
+      },
+    );
+    return false;
+  }
+
   return true;
 }
 
